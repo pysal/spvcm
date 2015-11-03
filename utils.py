932e493c14ac936c3e7c59589fc53f68d770b605
levi.john.wolf@gmail.com <ljw2@asu.edu>
2015-11-03 14:31:01 -0700

    add timing code

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from scipy import sparse as spar
 import numpy as np
 from numpy import linalg as la
 from scipy.sparse import linalg as spla
+import time
 
 def logdet(M):
     if spar.issparse(M):
@@ -10,3 +11,13 @@ def logdet(M):
     else:
         ldet = la.slogdet(M)
     return ldet
+
+def time_gibbs(Gibbs, n=100):
+    times = {k:[] for k in Gibbs.var_names}
+    for it in range(n*len(Gibbs.var_names)):
+        currname = Gibbs.var_names[it % len(Gibbs.var_names)]
+        s = time.time()
+        Gibbs.next()
+        times[currname].append(time.time() - s)
+    return times
+
