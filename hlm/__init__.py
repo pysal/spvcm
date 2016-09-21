cb246926131c3b46fc3e0a49cfbfa3f7083e6146
ljwolf <levi.john.wolf@gmail.com>
2016-09-20 23:32:28 -0700

    fix svcp location

--- hlm/__init__.py
+++ hlm/__init__.py
@@ -1,7 +1,6 @@
 from . import both_levels as both #okay, because it's not * all the way down :)
 from . import upper_level as upper
 from . import lower_level as lower
-from . import svcp as svcp
 from . import hierarchical as hier
 from .plotting.traces import plot_trace
 
@@ -9,7 +8,6 @@ _all = [v for v in both.__dict__.values() if isinstance(v, type)]
 _all.extend([v for v in upper.__dict__.values() if isinstance(v, type)])
 _all.extend([v for v in lower.__dict__.values() if isinstance(v, type)])
 _all.extend([v for v in hier.__dict__.values() if isinstance(v, type)])
-_all.extend([v for v in svcp.__dict__.values() if isinstance(v, type)]) 
 
-del (both_levels, upper_level, lower_level, svcp, hierarchical, 
+del (both_levels, upper_level, lower_level, hierarchical, 
      trace, plotting, utils, verify, steps, abstracts)
