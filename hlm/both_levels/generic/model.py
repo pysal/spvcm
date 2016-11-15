dispatch to the correct form of sampling (precision or covariance)
This changes the samplers to use a precision or a covariance, depending
on which is faster. For the SMA samplers, using the covariance provides
a faster sparse log determinant, since the covariance
(I + lambda W)(I + lambda W)'
is sparse. For the SE samplers, using the precision provides a faster
sparse log determinant, since the precision is:
(I - lambda W)'(I - lambda W)
which is not as sparse as the SMA form, but is still quite sparse.

hlm/both_levels/generic/model.py
@@ -6,12 +6,13 @@ import copy
 
 from numpy import linalg as la
 from warnings import warn as Warn
-from .sample import logp_rho, logp_lambda
+from .sample import logp_rho_cov, logp_lambda_cov
 from ...abstracts import Sampler_Mixin, Hashmap, Trace
 from ... import verify
 from ... steps import Metropolis, Slice
 from ... import priors
 from ...utils import speigen_range, ind_covariance, chol_mvn
+from pysal.spreg.utils import spdot
 
 SAMPLERS = ['Alphas', 'Betas', 'Sigma2', 'Tau2', 'Lambda', 'Rho']
 
@@ -56,7 +57,9 @@ class Base_Generic(Sampler_Mixin):
         
         ## Covariance, computing the starting values
         self.state.Psi_1 = ind_covariance
+        self.state.Psi_1i = ind_covariance
         self.state.Psi_2 = ind_covariance
+        self.state.Psi_2i = ind_covariance
         
         self.cycles = 0
         
@@ -111,16 +114,8 @@ class Base_Generic(Sampler_Mixin):
         st.Sigma2_an = self.state.N / 2 + st.Sigma2_a0
         st.Tau2_an = self.state.J / 2 + st.Tau2_a0
         
-        st.PsiRho = st.Psi_1(st.Rho, st.W)
-        st.PsiLambda = st.Psi_2(st.Lambda, st.M)
-
-        st.PsiSigma2 = st.PsiRho.dot(st.In * st.Sigma2)
-        st.PsiSigma2i = la.inv(st.PsiSigma2)
-        st.PsiTau2 = st.PsiLambda.dot(st.Ij * st.Tau2)
-        st.PsiTau2i = la.inv(st.PsiTau2)
-
-        st.PsiRhoi = la.inv(st.PsiRho)
-        st.PsiLambdai = la.inv(st.PsiLambda)
+        st.PsiRhoi = st.Psi_1i(st.Rho, st.W)
+        st.PsiLambdai = st.Psi_2i(st.Lambda, st.M)
         
         ## Data invariants
         st.XtX = np.dot(self.state.X.T, self.state.X)
@@ -128,6 +123,9 @@ class Base_Generic(Sampler_Mixin):
 
         st.DeltaAlphas = np.dot(st.Delta, st.Alphas)
         st.XBetas = np.dot(st.X, st.Betas)
+            
+        st.initial_values = Hashmap(Betas = st.Betas, Alphas=st.Alphas, Rho=st.Rho, Lambda=st.Lambda,
+                                    Sigma2=st.Sigma2, Tau2=st.Tau2)
 
 
     def _setup_configs(self, Lambda_method = 'met', Lambda_configs = None,
@@ -149,30 +147,32 @@ class Base_Generic(Sampler_Mixin):
             Rho_configs = copy.deepcopy(uncaught)
 
         if Lambda_method.lower().startswith('met'):
-            method = Metropolis
+            Lambda_method = Metropolis
             Lambda_configs['jump'] = Lambda_configs.pop('jump', .5)
             Lambda_configs['max_tuning'] = Lambda_configs.pop('tuning',0)
         elif Lambda_method.lower().startswith('slice'):
-            method = Slice
+            Lambda_method = Slice
             Lambda_configs['width'] = Lambda_configs.pop('width', .5)
         else:
             raise Exception('Sample method for Lambda not understood:\n{}'
                             .format(Lambda_method))
         if Rho_method.lower().startswith('met'):
-            method = Metropolis
+            Rho_method = Metropolis
             Rho_configs['jump'] = Rho_configs.pop('jump', .5)
             Rho_configs['max_tuning'] = Rho_configs.pop('tuning',0)
 
         elif Rho_method.lower().startswith('slice'):
-            method = Slice
+            Rho_method = Slice
             Rho_configs['width'] = Rho_configs.pop('width', .5)
         else:
             raise Exception('Sample method for Rho not understood:\n{}'
                             .format(Rho_method))
+        print('Rho method, configs: {}'.format(Rho_method, Rho_configs))
+        print('Lambda method, configs: {}'.format(Lambda_method, Lambda_configs))
 
         self.configs = Hashmap()
-        self.configs.Rho = method('Rho', logp_rho, **Rho_configs)
-        self.configs.Lambda = method('Lambda', logp_lambda, **Lambda_configs)
+        self.configs.Rho = Rho_method('Rho', logp_rho_cov, **Rho_configs)
+        self.configs.Lambda = Lambda_method('Lambda', logp_lambda_cov, **Lambda_configs)
 
     def _setup_truncation(self, Rho_min=None, Rho_max = None,
                           Lambda_min = None, Lambda_max = None):
@@ -242,13 +242,15 @@ class Base_Generic(Sampler_Mixin):
         ### N(Sb, S) where
         ### S = (X' Sigma^{-1}_Y X + S_0^{-1})^{-1}
         ### b = X' Sigma^{-1}_Y (Y - Delta Alphas) + S^{-1}\mu_0
-        covm_update = st.X.T.dot(st.PsiRhoi).dot(st.X) / st.Sigma2
+        covm_update = spdot(st.X.T, spdot(st.PsiRhoi, st.X)) / st.Sigma2
         covm_update += st.Betas_cov0i
+        covm_update = np.asarray(covm_update)
         covm_update = la.inv(covm_update)
     
         resids = st.Y - st.Delta.dot(st.Alphas)
-        XtSresids = st.X.T.dot(st.PsiRhoi).dot(resids) / st.Sigma2
+        XtSresids = spdot(st.X.T, spdot(st.PsiRhoi, resids)) / st.Sigma2
         mean_update = XtSresids + st.Betas_cov0i.dot(st.Betas_mean0)
+        mean_update = np.asarray(mean_update)
         mean_update = np.dot(covm_update, mean_update)
         st.Betas = chol_mvn(mean_update, covm_update)
         st.XBetas = np.dot(st.X, st.Betas)
@@ -261,12 +263,14 @@ class Base_Generic(Sampler_Mixin):
         ### Where
         ### S = (Delta'Sigma_Y^{-1}Delta + Sigma_Alpha^{-1})^{-1}
         ### b = (Delta'Sigma_Y^{-1}(Y - X\beta) + 0)
-        covm_update = st.Delta.T.dot(st.PsiRhoi).dot(st.Delta) / st.Sigma2
+        covm_update = spdot(st.Delta.T, spdot(st.PsiRhoi, st.Delta)) / st.Sigma2
         covm_update += st.PsiLambdai / st.Tau2
+        covm_update = np.asarray(covm_update)
         covm_update = la.inv(covm_update)
     
         resids = st.Y - st.XBetas
-        mean_update = st.Delta.T.dot(st.PsiRhoi).dot(resids) / st.Sigma2
+        mean_update = spdot(st.Delta.T, spdot(st.PsiRhoi, resids)) / st.Sigma2
+        mean_update = np.asarray(mean_update)
         mean_update = np.dot(covm_update, mean_update)
         st.Alphas = chol_mvn(mean_update, covm_update)
         st.DeltaAlphas = np.dot(st.Delta, st.Alphas)
@@ -276,7 +280,7 @@ class Base_Generic(Sampler_Mixin):
         ###                            \dot P(Tau2) \dot P(\lambda)
         ### is
         ### IG(J/2 + a0, u'(\Psi(\lambda))^{-1}u * .5 + b0)
-        bn = st.Alphas.T.dot(st.PsiLambdai).dot(st.Alphas) * .5 + st.Tau2_b0
+        bn = spdot(st.Alphas.T, spdot(st.PsiLambdai, st.Alphas)) * .5 + st.Tau2_b0
         st.Tau2 = stats.invgamma.rvs(st.Tau2_an, scale=bn)
         
         ### Sample the response aspatial variance parameter
@@ -285,7 +289,7 @@ class Base_Generic(Sampler_Mixin):
         ### IG(N/2 + a0, eta'Psi(\rho)^{-1}eta * .5 + b0)
         ### Where eta is the linear predictor, Y - X\beta + \DeltaAlphas
         eta = st.Y - st.XBetas - st.DeltaAlphas
-        bn = eta.T.dot(st.PsiRhoi).dot(eta) * .5 + st.Sigma2_b0
+        bn = spdot(eta.T, spdot(st.PsiRhoi, eta)) * .5 + st.Sigma2_b0
         st.Sigma2 = stats.invgamma.rvs(st.Sigma2_an, scale=bn)
     
         ### Sample the spatial components using metropolis-hastings
@@ -295,19 +299,14 @@ class Base_Generic(Sampler_Mixin):
         ###  * 1/(emax-emin)
         st.Rho = self.configs.Rho(st)
         
-        st.PsiRho = st.Psi_1(st.Rho, st.W)
-        st.PsiSigma2 = st.PsiRho.dot(st.In*st.Sigma2)
-        st.PsiSigma2i = la.inv(st.PsiSigma2)
-        st.PsiRhoi = la.inv(st.PsiRho)
+        st.PsiRhoi = st.Psi_1i(st.Rho, st.W) #BtB or CiCti
             
         ### P(Psi(\rho) | . ) \propto L(Y | .) \dot P(\rho)
         ### is
         ### |Psi(rho)|^{-1/2} exp(1/2(eta'Psi(rho)^{-1}eta * Sigma2^{-1})) * 1/(emax-emin)
         st.Lambda = self.configs.Lambda(st)
-        st.PsiLambda = st.Psi_2(st.Lambda, st.M)
-        st.PsiTau2 = st.PsiLambda.dot(st.Ij * st.Tau2)
-        st.PsiTau2i = la.inv(st.PsiTau2)
-        st.PsiLambdai = la.inv(st.PsiLambda)
+
+        st.PsiLambdai = st.Psi_2i(st.Lambda, st.M)
 
 class Generic(Base_Generic):
     """

hlm/both_levels/generic/sample.py
@@ -1,13 +1,14 @@
 import numpy as np
 import numpy.linalg as la
-from ...utils import splogdet
+from ...utils import splogdet, spsolve
 from ...steps import metropolis
+from pysal.spreg.utils import spdot
 
 #############################
 # SPATIAL SAMPLE METHODS    #
 #############################
 
-def logp_rho(state, val):
+def logp_rho_cov(state, val):
     """
     The logp for lower-level spatial parameters in this case has the same
     form as a multivariate normal distribution, sampled over the variance matrix, rather than over y.
@@ -19,15 +20,14 @@ def logp_rho(state, val):
         return np.array([-np.inf])
     
     PsiRho = st.Psi_1(val, st.W)
-    PsiRhoi = la.inv(PsiRho)
     logdet = splogdet(PsiRho)
     
     eta = st.Y - st.XBetas - st.DeltaAlphas
-    kernel = eta.T.dot(PsiRhoi).dot(eta) / st.Sigma2
+    kernel = spdot(eta.T, spsolve(PsiRho, eta)) / st.Sigma2
 
     return -.5*logdet -.5 * kernel + st.Log_Rho0(val)
 
-def logp_lambda(state, val):
+def logp_lambda_cov(state, val):
     """
     The logp for upper level spatial parameters in this case has the same form
     as a multivariate normal distribution, sampled over the variance matrix,
@@ -40,10 +40,47 @@ def logp_lambda(state, val):
         return np.array([-np.inf])
 
     PsiLambda = st.Psi_2(val, st.M)
-    PsiLambdai = la.inv(PsiLambda)
 
     logdet = splogdet(PsiLambda)
 
-    kernel = st.Alphas.T.dot(PsiLambdai).dot(st.Alphas) / st.Tau2
+    kernel = spdot(st.Alphas.T, spsolve(PsiLambda, st.Alphas)) / st.Tau2
 
     return -.5*logdet - .5*kernel + st.Log_Lambda0(val)
+
+def logp_lambda_prec(state, val):
+    """
+    Compute the log likelihood of the upper-level spatial correlation parameter using 
+    sparse operations and the precision matrix, rather than the covariance matrix. 
+    """
+    st = state
+
+    if (val < st.Lambda_min) or (val > st.Lambda_max):
+        return np.array([-np.inf])
+
+    PsiLambdai = st.Psi_2i(val, st.M, sparse=True)
+    logdet = -splogdet(PsiLambdai) #negative because precision
+
+    kernel = spdot(spdot(st.Alphas.T, PsiLambdai), st.Alphas) / st.Tau2
+
+    return -.5 * logdet - .5 * kernel + st.Log_Lambda0(val)
+
+def logp_rho_prec(state, val):
+    """
+    Compute the log likelihood of the lower-level spatial correlation parameter using
+    sparse operations and the precision matrix, rather than the covariance matrix. 
+    """
+    st = state
+
+    if (val < st.Rho_min) or (val > st.Rho_max):
+        return np.array([-np.inf])
+
+    PsiRhoi = st.Psi_1i(val, st.W, sparse=True)
+    logdet = -splogdet(PsiRhoi)
+
+    eta = st.Y - st.XBetas - st.DeltaAlphas
+
+    kernel = spdot(spdot(eta.T, PsiRhoi), eta) / st.Sigma2
+
+    return -.5 * logdet - .5 * kernel + st.Log_Rho0(val)
+
+

hlm/both_levels/sma_se/model.py
@@ -1,5 +1,6 @@
 from ..generic import Base_Generic
-from ...utils import se_covariance, sma_covariance
+from ..generic.sample import logp_lambda_prec
+from ...utils import se_covariance, sma_covariance, se_precision, sma_precision
 from ... import verify
 import numpy as np
 
@@ -19,8 +20,13 @@ class Base_SMASE(Base_Generic):
         st = self.state
         self.state.Psi_1 = sma_covariance
         self.state.Psi_2 = se_covariance
+        self.state.Psi_1i = sma_precision
+        self.state.Psi_2i = se_precision
 
         st.Rho_min, st.Rho_max = -st.Rho_max, -st.Rho_min
+
+        self.configs.Lambda.logp = logp_lambda_prec
+        
         try:
             self.sample(n_samples, n_jobs=n_jobs)
         except (np.linalg.LinAlgError, ValueError) as e:

hlm/upper_level/se/model.py
@@ -8,7 +8,8 @@ import numpy.linalg as la
 from ...both_levels.generic import Base_Generic
 from ...both_levels.generic.model import SAMPLERS as generic_parameters
 from ... import verify
-from ...utils import se_covariance, ind_covariance, splogdet, chol_mvn
+from ...utils import se_covariance, se_precision, ind_covariance, splogdet, chol_mvn
+from pysal.spreg.utils import spdot
 
 
 
@@ -35,7 +36,9 @@ class Base_Upper_SE(Base_Generic):
                                             starting_values=starting_values,
                                             truncation=truncation)
         self.state.Psi_1 = ind_covariance
+        self.state.Psi_1i = ind_covariance
         self.state.Psi_2 = se_covariance
+        self.state.Psi_2i = se_precision
         original_traced = copy.deepcopy(self.traced_params)
         to_drop = [k for k in original_traced if (k not in SAMPLERS and k in generic_parameters)]
         self.traced_params = copy.deepcopy(SAMPLERS)
@@ -80,11 +83,13 @@ class Base_Upper_SE(Base_Generic):
         ### b = (Delta'Sigma_Y^{-1}(Y - X\beta) + 0)
         covm_update = st.Delta.T.dot(st.Delta) / st.Sigma2
         covm_update += st.PsiLambdai / st.Tau2
+        covm_update = np.asarray(covm_update)
         covm_update = la.inv(covm_update)
 
         resids = st.Y - st.XBetas
         mean_update = st.Delta.T.dot(resids) / st.Sigma2
         mean_update = np.dot(covm_update, mean_update)
+        mean_update = np.asarray(mean_update)
         st.Alphas = chol_mvn(mean_update, covm_update)
         st.DeltaAlphas = np.dot(st.Delta, st.Alphas)
 
@@ -93,7 +98,7 @@ class Base_Upper_SE(Base_Generic):
         ###                            \dot P(Tau2) \dot P(\lambda)
         ### is
         ### IG(J/2 + a0, u'(\Psi(\lambda))^{-1}u * .5 + b0)
-        bn = st.Alphas.T.dot(st.PsiLambdai).dot(st.Alphas) * .5 + st.Tau2_b0
+        bn = spdot(st.Alphas.T, spdot(st.PsiLambdai, st.Alphas)) * .5 + st.Tau2_b0
         st.Tau2 = stats.invgamma.rvs(st.Tau2_an, scale=bn)
 
         ### Sample the response aspatial variance parameter
@@ -109,8 +114,7 @@ class Base_Upper_SE(Base_Generic):
         ### is
         ### |Psi(rho)|^{-1/2} exp(1/2(eta'Psi(rho)^{-1}eta * Sigma2^{-1})) * 1/(emax-emin)
         st.Lambda = self.configs.Lambda(st)
-        st.PsiLambda = st.Psi_2(st.Lambda, st.M)
-        st.PsiLambdai = la.inv(st.PsiLambda)
+        st.PsiLambdai = st.Psi_2i(st.Lambda, st.M)
 
 class Upper_SE(Base_Upper_SE):
     """