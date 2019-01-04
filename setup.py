331ed9672531252641b763b83d3a14e8b2216fba
ljwolf <levi.john.wolf@gmail.com>
2019-01-04 19:28:47 +0000

    rev for post-release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(fh, 'r') as f:
     long_description = f.read()
 
 setup(name='spvcm',
-      version='0.2.1',
+      version='0.2.1post1',
       long_description = long_description,
       description='Fit spatial multilevel models and diagnose convergence',
       url='https://github.com/ljwolf/spvcm',
